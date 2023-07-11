31aab768efaa5c7aaa13fda75ab7c5240e77bb4d
gksehfapd <1ehfapd@gmail.com>
2023-07-12 02:03:16 +0900

    outlet

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import Home from './screens/Home'
 import NotFound from './screens/NotFound'
 import ErrorComponent from './components/ErrorComponent'
 import User from './screens/users/User'
+import Followers from './screens/users/Followers'
 
 const router = createBrowserRouter([
 	{
@@ -23,7 +24,13 @@ const router = createBrowserRouter([
 			},
 			{
 				path: 'users/:userId',
-				element: <User />
+				element: <User />,
+				children: [
+					{
+						path: 'followers',
+						element: <Followers />
+					}
+				]
 			}
 		],
 		errorElement: <NotFound />
--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom'
+import { Link, Outlet, useParams } from 'react-router-dom'
 import { users } from '../../db'
 
 function User() {
@@ -6,7 +6,14 @@ function User() {
 
 	return (
 		<div>
-			User with id {userId} is name: {users[Number(userId) - 1].name}
+			<h1>
+				User with id {userId} is name: {users[Number(userId) - 1].name}
+			</h1>
+			<hr />
+			{/* to='/followers' 는 절대경로
+				to='followers' 는 상대경로 */}
+			<Link to="followers">See followers</Link>
+			<Outlet />
 		</div>
 	)
 }
